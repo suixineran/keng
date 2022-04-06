7578ceb5ce95f09dcb89ab681ed14bb6331c62d9
weizhen <1027638598@qq.com>
2022-04-06 09:34:46 +0800

    动画的事件触发机制

--- readme.js
+++ readme.js
@@ -1,4 +1,20 @@
 /*
 * 主要记录前端开发过程中  跟常规理解不一致的点
 *
+*
+* require  的使用
+*
+* echarts 的api 和属性  的使用  legend
+*
+* canvas  的使用
+*
+* vue 的数组 什么情况下 才必须用 到 $set
+*
+*
+* setTimeout  不删除 会怎么样？？ 一个页面会有很多吗？ 怎么查看
+*
+* 性能优化方面的 逻辑 有哪些
+*
+* 动画 animation  直接写到style 中不生效？？？？？  用animation-delay 生效
 * */
+
